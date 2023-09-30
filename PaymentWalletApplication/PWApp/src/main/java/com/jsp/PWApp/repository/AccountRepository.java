b76d650a7a904e88c9394bcee71b6d08f51d6b8b
Srisowmya <srisowmya@eligait.com>
2023-09-30 22:45:26 +0530

    first commit

--- PaymentWalletApplication/PWApp/src/main/java/com/jsp/PWApp/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jsp.PWApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jsp.PWApp.dto.Account;
-
-public interface AccountRepository extends JpaRepository<Account, Integer> {
-
-}
